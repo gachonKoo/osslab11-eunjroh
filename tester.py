bbd8b8965f618f164d3b518df21bab8bdce0aba9
eunjroh <eunjroh@gachon.ac.kr>
2024-11-16 22:32:26 +0900

    tester.py

--- tester.py
+++ tester.py
@@ -1,45 +1,13 @@
-name: Autograding Tests
-'on':
-  - push
-  - repository_dispatch
-permissions:
-  checks: write
-  actions: read
-  contents: read
-jobs:
-  run-autograding-tests:
-    runs-on: ubuntu-latest
-    if: github.actor != 'github-classroom[bot]'
-    steps:
-      # 1. 레포지토리 코드 체크아웃
-      - name: Checkout code
-        uses: actions/checkout@v4
+python tester.py
+import math
 
-      # 2. Python 환경 설정
-      - name: Set up Python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.x'
+# 반지름 값 설정
+radius = 5.0
 
-      # 3. 테스트 실행
-      - name: Run Test
-        id: test
-        uses: classroom-resources/autograding-io-grader@v1
-        with:
-          test-name: test
-          setup-command: ""
-          command: python tester.py
-          input: ""
-          expected-output: |-
-            c = 5.0
-            area = 314.1592653589793
-          comparison-method: contains
-          timeout: 10
+# 원의 둘레와 면적 계산
+circumference = 2 * math.pi * radius  # 둘레 계산
+area = math.pi * radius ** 2  # 면적 계산
 
-      # 4. 결과 보고
-      - name: Autograding Reporter
-        uses: classroom-resources/autograding-grading-reporter@v1
-        env:
-          TEST_RESULTS: "${{steps.test.outputs.result}}"
-        with:
-          runners: test
+# 결과 출력
+print(f"c = {radius}")
+print(f"area = {area}")
